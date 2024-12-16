6edd8e2d9422d5218090c84ec09017825068bbfb
Tayyab Riaz <tayyabriyax@gmail.com>
2024-12-16 12:54:23 +0500

    Set up Environment for Pix Flow

---
+++ src/main/java/com/practice/pixflow/PixflowApplication.java
@@ -0,0 +1,13 @@
+package com.practice.pixflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/pixflow/PixflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.pixflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
