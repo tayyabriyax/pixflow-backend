08a5394a5b10eb68306cb19fb89955261902f7fc
Tayyab Riaz <tayyabriyax@gmail.com>
2024-12-17 12:31:12 +0500

    Use Lombok for Getters and Setters

--- src/main/java/com/practice/pixflow/dto/SignUpDTO.java
+++ src/main/java/com/practice/pixflow/dto/SignUpDTO.java
@@ -1,5 +1,12 @@
 package com.practice.pixflow.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignUpDTO {
 
     private String userName;
@@ -8,27 +15,4 @@ public class SignUpDTO {
 
     private String password;
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/practice/pixflow/entity/UserEntity.java
+++ src/main/java/com/practice/pixflow/entity/UserEntity.java
@@ -1,8 +1,14 @@
 package com.practice.pixflow.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "user_name"))
 public class UserEntity {
 
@@ -25,51 +31,4 @@ public class UserEntity {
     @Column(name = "about")
     private String about;
 
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public void setProfilePic(String profilePic) {
-        this.profilePic = profilePic;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
