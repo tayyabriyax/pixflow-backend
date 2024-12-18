ad3b3d7e4d091421321f32073ab21ca7b76fb0fb
Tayyab Riaz <tayyabriyax@gmail.com>
2024-12-18 16:58:16 +0500

    Add Spring Security to the Project

--- src/main/java/com/practice/pixflow/repository/UserRepository.java
+++ src/main/java/com/practice/pixflow/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.practice.pixflow.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+
+    UserEntity findUserByUserName(String userName);
+
 }
