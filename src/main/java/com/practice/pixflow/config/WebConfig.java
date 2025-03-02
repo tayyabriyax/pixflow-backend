5335b073cf117348215315aae0c5ced8c6b7f907
Tayyab Riaz <tayyabriyax@gmail.com>
2025-03-02 12:01:44 +0500

    Allow All Origins

--- src/main/java/com/practice/pixflow/config/WebConfig.java
+++ src/main/java/com/practice/pixflow/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
--- src/main/java/com/practice/pixflow/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practice.pixflow.repository;
-
-import com.practice.pixflow.entity.NotificationEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NotificationRepository extends CrudRepository<NotificationEntity, Integer> {
-}
